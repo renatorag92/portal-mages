dd2f5d68f53dcdbf943f710e073d87d904d96df5
renatorag <araujo.renato@escolar.ifrn.edu.br>
2026-08-29 09:46:47 -0300

    feat: ajusta retorno __str__ no modelo Acao

--- actions/models.py
+++ actions/models.py
@@ -1,41 +1,45 @@
 from django.db import models
 
-STATUS_CHOICES = [
-    ('planejado', 'Planejado'),
-    ('em_preparacao', 'Em preparação'),
-    ('em_execucao', 'Em execução'),
-    ('em_validacao', 'Em validação'),
-    ('concluido', 'Concluído'),
-    ('cancelado', 'Cancelado'),
-    ('alta', 'Alta'),
-    ('media', 'Média'),
-    ('baixa', 'Baixa'),]
-
 class Acao(models.Model):
+
+    class Status(models.TextChoices):
+        PLANEJADO = 'planejado', 'Planejado'
+        EM_PREPARACAO = 'em_preparacao', 'Em preparação'
+        EM_EXECUCAO = 'em_execucao', 'Em execução'
+        EM_VALIDACAO = 'em_validacao', 'Em validação'
+        CONCLUIDO = 'concluido', 'Concluído'
+        CANCELADO = 'cancelado', 'Cancelado'
+
     codigo = models.CharField(max_length=10)
     nome = models.CharField(max_length=100)
     eixo = models.CharField(max_length=100)
-    prioridade = models.CharField(max_length=20, choices=STATUS_CHOICES,)
+    prioridade = models.CharField(max_length=20, choices=Status,)
     responsavel = models.CharField(max_length=100)
     data_inicio = models.DateField()
     data_fim = models.DateField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='planejado')            
+    status = models.CharField(max_length=20, choices=Status, default='planejado')            
     custo = models.DecimalField(max_digits=10, decimal_places=2)
     observacao = models.TextField(blank=True, null=True)
 
 class Etapa(models.Model):
+
+    class Status(models.TextChoices):
+        BAIXA = 'baixa', 'Baixa'
+        MEDIA = 'media', 'Média'
+        ALTA = 'alta', 'Alta'
+
     acao = models.ForeignKey(Acao, on_delete=models.CASCADE, related_name='etapas')
     etapa = models.CharField(max_length=100)
     responsavel = models.CharField(max_length=100)
     data_inicio = models.DateField()
     data_fim = models.DateField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='planejado')
-    prioridade = models.IntegerField()
+    status = models.CharField(max_length=20, choices=Status, default='planejado')
+    prioridade = models.CharField(max_length=20, choices=Status, default='')
     observacao = models.TextField(blank=True, null=True)
 
-# Retorna o nome da ação quando for chamado o objeto
+# Retorna o codigo e o nome da ação quando for chamado o objeto
 def __str__(self):
-    return self.nome
+    return self.codigo + ' - ' + self.nome
 # Retorna o nome da etapa quando for chamado o objeto
 def __str__(self):
     return self.etapa
