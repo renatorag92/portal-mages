f0a95eb0780e062b06fbfedcd696e926127c5bca
renatorag <araujo.renato@escolar.ifrn.edu.br>
2026-08-30 15:20:06 -0300

    feat: adiciona atualizacao de status das acoes via kanban

--- actions/urls.py
+++ actions/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.acao_list_view, name='acao_list'),
+    path('actions/<int:acao_id>/atualizar_status/', views.atualizar_status_acao, name='atualizar_status_acao'),
 ]
 
