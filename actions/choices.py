35262d95f10340e0524bafddf841ec44cadf713d
renatorag <araujo.renato@escolar.ifrn.edu.br>
2026-08-30 00:10:46 -0300

    feat: cria a estrutura do painel Kanban com colunas de status

---
+++ actions/choices.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Status_eixo(models.TextChoices):
+    CADASTRO_TRIBUTARIO = 'cadastro_tributario', 'Cadastro Tributário'
+    FISCALIZACAO_TRIBUTARIA = 'fiscalizacao_tributaria', 'Fiscalização Tributária'
+    ARRECADACAO_E_COBRANCA = 'arrecadacao_e_cobranca', 'Arrecadação e Cobrança'
+    MODERNIZACAO_E_TECNOLOGIA = 'modernizacao_e_tecnologia', 'Modernização e Tecnologia'
+    GOVERNANCA_E_LEGISLACAO = 'governanca_e_legislacao', 'Governança e Legislação'       
+
+class Status_status(models.TextChoices):
+        PLANEJADO = 'planejado', 'Planejado'
+        EM_PREPARACAO = 'em_preparacao', 'Em preparação'
+        EM_EXECUCAO = 'em_execucao', 'Em execução'
+        EM_VALIDACAO = 'em_validacao', 'Em validação'
+        CONCLUIDO = 'concluido', 'Concluído'
+        CANCELADO = 'cancelado', 'Cancelado'
+
+class Status_prioridade(models.TextChoices):
+        BAIXA = 'baixa', 'Baixa'
+        MEDIA = 'media', 'Média'
+        ALTA = 'alta', 'Alta'
--- mages/urls.py
+++ mages/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('actions/', include('actions.urls')),  # Inclui as URLs do aplicativo 'actions'
 ]
